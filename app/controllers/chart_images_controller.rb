b4738c5a7f899d8e7b1fda7e500b37e53fd12c60
Jay OConnor <jay@bellycard.com>
2014-01-03 15:33:39 -0600

    output the png from the png data memcache the data

--- app/controllers/chart_images_controller.rb
+++ app/controllers/chart_images_controller.rb
@@ -1,14 +1,21 @@
 class ChartImagesController < ApplicationController
   def create
-    send_file chart_image.file_path
-    chart_image.close
+    send_data Base64.decode64(chart_data), :type => 'image/png', :disposition => 'inline', :filename => 'chart.png'
   end
   alias :index :create
 
   private
 
-  def chart_image
-    @chart_image ||= ChartImage.new(input: params[:input], width: params[:width])
+  def chart_data
+    cache_key = params.flatten.to_s.parameterize.hash
+    # read from cache for performance
+    @chart_data = Rails.cache.read(cache_key)
+    if @chart_data.nil?
+      image ||= ChartImage.new(input: params[:input], width: params[:width])
+      @chart_data = image.data
+      Rails.cache.write(cache_key, image.data)
+    end
+    @chart_data
   end
 
 end
